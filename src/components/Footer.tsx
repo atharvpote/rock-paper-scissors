9f137d7d0908503843813a6db3d3ff870bbf4d77
Atharv Pote <atharvpote@gmail.com>
2022-09-30 20:57:51 +0530

    Added Footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+  return (
+    <div className="mx-auto max-w-6xl">
+      <footer className="flex justify-center pb-8 md:mr-6 md:justify-end md:pb-6">
+        <button className="rounded-xl border-2 border-white border-opacity-50 px-8 py-2 text-lg uppercase tracking-widest text-white">
+          rules
+        </button>
+      </footer>
+    </div>
+  );
+}

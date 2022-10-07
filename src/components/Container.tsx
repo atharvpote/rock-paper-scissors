e49e7df30e3b349b0de6b0f2bc64ef174182f5ea
Atharv Pote <atharvpote@gmail.com>
2022-10-07 08:13:58 +0530

    Some layout fixes.

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,17 +1,11 @@
-import { useLocation } from "react-router-dom";
-
 type Props = {
   children: React.ReactNode;
 };
 
 export default function Container(props: Props): JSX.Element {
-  const location = useLocation();
-
   return (
     <div
-      className={`grid min-h-[calc(100vh_-_144px_-_80px)] place-content-center md:min-h-[calc(100vh_-_148px_-_72px)] ${
-        location.pathname === "/result" ? "adjust-to-height" : ""
-      }`}
+      className={`grid min-h-[calc(100vh_-_144px_-_80px)] place-content-center py-20 md:min-h-[calc(100vh_-_148px_-_72px)] md:py-28`}
     >
       {props.children}
     </div>
